fac4e7f344edb8894990ac5e17967e1c39f97f5a
Mohammadreza Molaei <mrmolaei.ir@gmail.com>
2023-01-02 23:29:00 +0330

    Load assets only on plugin's pages

--- includes/services/Assets.php
+++ includes/services/Assets.php
@@ -9,13 +9,17 @@ use WP_Palette\BaseClass;
 class Assets extends BaseClass
 {
 
-	public function register() {
+	public function register()
+	{
 		add_action( 'admin_enqueue_scripts', [ $this, 'adminAssets' ] );
 	}
 
-	public function adminAssets() {
-		wp_enqueue_script( 'wp-palette-iro-library', $this->plugin_url . 'assets/js/iro.min.js' );
-		wp_enqueue_script( 'wp-palette-admin-scripts', $this->plugin_url . 'assets/js/scripts.js' );
-		wp_enqueue_style( 'wp-palette-admin-styles', $this->plugin_url . 'assets/css/main.css' );
+	public function adminAssets()
+	{
+		if ( is_admin() && isset( $_REQUEST['page'] ) && ( substr( $_REQUEST['page'], 0, 10 ) == "wp_palette" ) ) {
+			wp_enqueue_script( 'wp-palette-iro-library', $this->plugin_url . 'assets/js/iro.min.js' );
+			wp_enqueue_script( 'wp-palette-admin-scripts', $this->plugin_url . 'assets/js/scripts.js' );
+			wp_enqueue_style( 'wp-palette-admin-styles', $this->plugin_url . 'assets/css/main.css' );
+		}
 	}
 }
