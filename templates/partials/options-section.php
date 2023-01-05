fad013d759b3a0d400b6f435529ecf9a32f96a75
Mohammadreza Molaei <mrmolaei.ir@gmail.com>
2023-01-05 20:22:39 +0330

    Add a table wrapper to handle overflows

--- templates/partials/options-section.php
+++ templates/partials/options-section.php
@@ -11,50 +11,52 @@ $colors       = isset( $palette_data['colors'] ) && $palette_data['colors'] ? $p
     <div class="c-grid__col">
         <h3><?php _e( 'Colors', WP_PALETTE_TEXT_DOMAIN ); ?></h3>
         <div class="c-card">
-            <table class="c-table s-table">
-                <thead>
-                <tr>
-                    <th><?php _e('Name', WP_PALETTE_TEXT_DOMAIN); ?></th>
-                    <th><?php _e('Color', WP_PALETTE_TEXT_DOMAIN); ?></th>
-                    <th class="has-input"><?php _e('RGB', WP_PALETTE_TEXT_DOMAIN); ?></th>
-                    <th class="has-input"><?php _e('HSL', WP_PALETTE_TEXT_DOMAIN); ?></th>
-                </tr>
-                </thead>
-                <tbody class="js-colors-table-body">
-				<?php foreach ( $colors as $key => $color ) { ?>
-                    <tr data-tr-id="<?php echo $key; ?>">
-                        <td>
-                            <button class="c-btn c-btn--remove js-remove-color">
-                                <span class="dashicons dashicons-no-alt"></span>
+            <div class="c-table-wrapper">
+                <table class="c-table s-table">
+                    <thead>
+                    <tr>
+                        <th><?php _e('Name', WP_PALETTE_TEXT_DOMAIN); ?></th>
+                        <th><?php _e('Color', WP_PALETTE_TEXT_DOMAIN); ?></th>
+                        <th class="has-input"><?php _e('RGB', WP_PALETTE_TEXT_DOMAIN); ?></th>
+                        <th class="has-input"><?php _e('HSL', WP_PALETTE_TEXT_DOMAIN); ?></th>
+                    </tr>
+                    </thead>
+                    <tbody class="js-colors-table-body">
+		            <?php foreach ( $colors as $key => $color ) { ?>
+                        <tr data-tr-id="<?php echo $key; ?>">
+                            <td>
+                                <button class="c-btn c-btn--remove js-remove-color">
+                                    <span class="dashicons dashicons-no-alt"></span>
+                                </button>
+                                <input class="c-color-name-input" name="wp_palette_data[colors][<?php echo $key; ?>][name]"
+                                       value="<?php echo $color['name']; ?>">
+                            </td>
+                            <td>
+                                <input name="wp_palette_data[colors][<?php echo $key; ?>][color]"
+                                       value="<?php echo $color['color']; ?>" class="js-color-picker-toggle c-color-input"
+                                       style="background-color: <?php echo $color['color']; ?>" type="text" readonly/>
+                            </td>
+                            <td>
+                                <input class="js-color-picker-rgb c-color-input c-color-input--data" value="<?php echo \WP_Palette\Helpers\Color::hexToRgb($color['color']) ?>" type="text" readonly/>
+                            </td>
+                            <td>
+                                <input class="js-color-picker-hsl c-color-input c-color-input--data" value="<?php echo \WP_Palette\Helpers\Color::hexToHSL($color['color']) ?>" type="text" readonly/>
+                            </td>
+                        </tr>
+		            <?php } ?>
+                    </tbody>
+                    <tfoot>
+                    <tr>
+                        <td colspan="4">
+                            <button class="js-add-color c-btn">
+                                <span class="dashicons dashicons-plus-alt2"></span>
+					            <?php _e('Add Color', WP_PALETTE_TEXT_DOMAIN); ?>
                             </button>
-                            <input class="c-color-name-input" name="wp_palette_data[colors][<?php echo $key; ?>][name]"
-                                   value="<?php echo $color['name']; ?>">
-                        </td>
-                        <td>
-                            <input name="wp_palette_data[colors][<?php echo $key; ?>][color]"
-                                   value="<?php echo $color['color']; ?>" class="js-color-picker-toggle c-color-input"
-                                   style="background-color: <?php echo $color['color']; ?>" type="text" readonly/>
-                        </td>
-                        <td>
-                            <input class="js-color-picker-rgb c-color-input c-color-input--data" value="<?php echo \WP_Palette\Helpers\Color::hexToRgb($color['color']) ?>" type="text" readonly/>
-                        </td>
-                        <td>
-                            <input class="js-color-picker-hsl c-color-input c-color-input--data" value="<?php echo \WP_Palette\Helpers\Color::hexToHSL($color['color']) ?>" type="text" readonly/>
                         </td>
                     </tr>
-				<?php } ?>
-                </tbody>
-                <tfoot>
-                <tr>
-                    <td colspan="4">
-                        <button class="js-add-color c-btn">
-                            <span class="dashicons dashicons-plus-alt2"></span>
-                            <?php _e('Add Color', WP_PALETTE_TEXT_DOMAIN); ?>
-                        </button>
-                    </td>
-                </tr>
-                </tfoot>
-            </table>
+                    </tfoot>
+                </table>
+            </div>
         </div>
     </div>
     <div class="c-grid__col">
